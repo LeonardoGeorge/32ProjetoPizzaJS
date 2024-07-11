fb98964b6cbd5314a5c28064ddb287a074f6e47f
Leonardo George <leonardogeorge30@gmail.com>
2024-07-11 17:48:01 -0300

    AulaFinalizada

--- pizza_base/script.js
+++ pizza_base/script.js
@@ -8,7 +8,7 @@ let modalQt = 1;
 // Variável que armazena no carrinho as pizza selecionadas no modal.
 let cart = [];
 
-// Variável que irá definar o id da pizza selecionada para todo o modal. 
+// Variável que irá definir o id da pizza selecionada para todo o modal. 
 let modalKey = 0;
 
 //Mapeando atravez do .map a array para ter acesso aos dados dos produtos.
@@ -73,16 +73,16 @@ pizzaJson.map((item, index) => {
 })
 
 // Criando a função cancelar pedido | fechar MODAL.
+qsAll('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=>{
+    item.addEventListener('click', closeModal);
+});
+
 function closeModal() {
     qs('.pizzaWindowArea').style.opacity = 0;
     setTimeout(()=>{
         qs('.pizzaWindowArea').style.display = 'none';
     }, 200)
 }
-
-qsAll('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=>{
-    item.addEventListener('click', closeModal);
-});
 
 // Criando função de aumentar e diminuir unidade do modal.
 qs('.pizzaInfo--qtmenos').addEventListener('click', ()=>{
@@ -116,9 +116,11 @@ qs('.pizzaInfo--addButton').addEventListener('click', ()=>{
     // Criando key que servira para procurar um identificado no carrinho para verificar se
     // já existe uma pizza com o mesmo id e tamanho, se não existir retornara -1,
     // se esxistir retornará um numero maior.
-    let key = cart.findIndex((item)=>item.identifier == identifier);
+    let key = cart.findIndex((item)=>{
+        return item.identifier == identifier;
+    });
 
-    // Verificando na pratica o restorno da identificação,
+    // Verificando na prática o retorno da identificação,
     // ou seja, se o retorno for maior que -1, ja existe o produto, 
     // tenho apenas que adicionar a quantidade, se não, tenho que dar um push(). 
     if(key > -1){
@@ -132,10 +134,104 @@ qs('.pizzaInfo--addButton').addEventListener('click', ()=>{
         })
     
     }
-    
+    updateCart();
     closeModal();
     
+});
+qs('.menu-openner').addEventListener('click', ()=>{
+    if(cart.length > 0) {
+        qs('aside').style.left = '0';
+    }
 })
+qs('.menu-closer').addEventListener('click', ()=>{
+    qs('aside').style.left = '100vw';
+    
+})
+
+//Função que trabalha fazendo o update de pedidos adicionados ao carrinho
+function updateCart() {
+    // Atualizar quantidades de itens adicionados | função Mobile
+    qs('.menu-openner').innerHTML = cart.length;
+
+    
+    // Verificando se existe algum pedido adicionado se sim
+    // abre o aside que mostra o pedido se não fecha o aside. 
+    if(cart.length > 0) {
+        qs('aside').classList.add('show');
+        qs('.cart').innerHTML = '';
+
+        let subtotal = 0;
+        let desconto = 0;
+        let total = 0;
+
+        // Loop que procura os pedidos adicinados para 
+        // preencher os dados do produtos no carrinho(aside).
+        for(let i in cart) {
+            // Variável para buscar dados do procuto pelo id.
+            let pizzaItem = pizzaJson.find((item)=>{
+                return item.id == cart[i].id;
+            });
+
+            // Calculando subtotais
+            subtotal += pizzaItem.price * cart[i].qt;
+            
+            //Clonando itens do modelo cart.
+            let cartItem = qs('.models .cart--item').cloneNode(true);
+            
+            // Adicionando ao carrinho na tela pelo append.
+            qs('.cart').append(cartItem);
+
+          // Declarando pizzaSizeName para compor a variável pizzaName.
+            let pizzaSizeName;
+            switch(cart[i].size) {
+                case 0:
+                    pizzaSizeName = '320g'
+                    break;
+                case 1: 
+                    pizzaSizeName = '530g'
+                    break;
+                case 2: 
+                    pizzaSizeName = '830g'
+                    break;
+            }
+          
+            // Variável para definir nome e tamanho da pizza.
+            let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`;
+          
+
+            // Preenchendo dados do pelido no carrinho.
+            cartItem.querySelector('img').src = pizzaItem.img;
+            cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
+            cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', ()=>{
+                if(cart[i].qt > 1){
+                    cart[i].qt --;
+                } else {
+                    cart.splice(i, 1);
+                }
+                updateCart()
+            });
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click', ()=>{
+                cart[i].qt ++;
+                updateCart();
+            });
+
+
+            // Calculo com desconto e calculo final.
+            desconto = subtotal * 0.1;
+            total = subtotal - desconto;
+
+            qs('.subtotal span:last-child').innerHTML = `R$ ${subtotal.toFixed(2)}`;
+            qs('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed(2)}`;
+            qs('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`;
+
+            
+        }
+    } else {
+        qs('aside').classList.remove('show');
+        qs('aside').style.left = '100vw';
+    }
+}
 
 
 
